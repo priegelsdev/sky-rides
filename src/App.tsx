8da5a564200311356fa40aadad6a7bfc2ee28da9
priegelsdev <priegelsdev@gmail.com>
2023-02-24 13:34:47 +0100

    initialize files

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
-function Test() {
-  return <div className="bg-blue-500">hello again</div>;
-}
-
-function About() {
-  return <div className="bg-green-800">About this page</div>;
-}
+import Home from './pages/Home';
+import About from './pages/About';
 
 // TODO: figure out app structure and where to actually put the routing; App.tsx probably correct, but not like this
 export default function App() {
   return (
     <BrowserRouter>
-      <Link to="/">Test</Link>
+      <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Routes>
-        <Route path="/" element={<Test />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
