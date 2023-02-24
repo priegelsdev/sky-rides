d2330bd4c4c6eb1a207aee3b68fb4e1ddaa2da1b
priegelsdev <priegelsdev@gmail.com>
2023-02-24 08:52:54 +0100

    add test component ot test out how route works

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+function Test() {
+  return <div className="bg-blue-500">hello again</div>;
+}
+
 // TODO: figure out app structure and where to actually put the routing; App.tsx probably correct, but not like this
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
-          <div className="bg-red-500">hi</div>
-        </Route>
+        <Route path="/" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
