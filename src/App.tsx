c0e98b2fd4784e395b5f561ede6cf3d16ece43a3
priegelsdev <priegelsdev@gmail.com>
2023-02-24 08:47:38 +0100

    add commenT

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// TODO: figure out app structure and where to actually put the routing; App.tsx probably correct, but not like this
 export default function App() {
-  return <div className="bg-red-500">Hi</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <div className="bg-red-500">hi</div>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
