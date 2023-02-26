c254b98e895d93d5d40700dc408a10e73563aceb
priegelsdev <priegelsdev@gmail.com>
2023-02-26 10:52:08 +0100

    host img to upload service to fix netlify deploy bug

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ createServer({
       price: 220,
       description:
         "The SkyHawk is a sleek and agile flying car that's perfect for zooming through the skies. With its powerful engines and advanced flight systems, the SkyHawk is the ultimate allround aerial machine.",
-      imageUrl: '/img/skyhawk.jpeg',
+      imageUrl: 'https://i.ibb.co/GpvhKMH/skyhawk.jpg',
       type: 'simple',
     });
     server.create('ride', {
@@ -21,7 +21,7 @@ createServer({
       price: 290,
       description:
         'The Nimbus is a high-performance flying car that combines speed and comfort in a stylish package. With its cutting-edge technology and luxurious interior, the Nimbus is the perfect way to travel in style.',
-      imageUrl: '/src/img/nimbus.png',
+      imageUrl: 'https://i.ibb.co/KKqFpzT/nimbus.png',
       type: 'luxury',
     });
     server.create('ride', {
@@ -30,7 +30,7 @@ createServer({
       price: 300,
       description:
         "The StarCruiser is a powerful and versatile flying car that can take you anywhere you want to go. Whether you're exploring new horizons or just cruising around town, the StarCruiser is the perfect ride.",
-      imageUrl: '/src/img/starcruiser.png',
+      imageUrl: 'https://i.ibb.co/449NS6L/starcruiser.png',
       type: 'rugged',
     });
     server.create('ride', {
@@ -39,7 +39,7 @@ createServer({
       price: 300,
       description:
         "The SkyChaser is a fast and nimble flying car that's perfect for thrill-seekers and adventurers. With its agile handling and sleek design, the SkyChaser is the ultimate aerial sports car.",
-      imageUrl: '/src/img/skychaser.png',
+      imageUrl: 'https://i.ibb.co/PFTR44g/skychaser.png',
       type: 'simple',
     });
     server.create('ride', {
@@ -48,7 +48,7 @@ createServer({
       price: 340,
       description:
         "The Explorer is a rugged and reliable flying car that's built to take on any challenge. With its advanced navigation systems and sturdy construction, the Explorer is the perfect choice for explorers and adventurers.",
-      imageUrl: '/src/img/explorer.png',
+      imageUrl: 'https://i.ibb.co/Dbc6301/explorer.png',
       type: 'rugged',
     });
     server.create('ride', {
@@ -57,7 +57,7 @@ createServer({
       price: 520,
       description:
         "The Winged Voyager is a luxurious flying car that's perfect for long-distance travel. With its spacious cabin and state-of-the-art entertainment systems, the Winged Voyager is the ultimate way to fly in comfort and style.",
-      imageUrl: '/src/img/voyager.png',
+      imageUrl: 'https://i.ibb.co/XSmYdV1/voyager.png',
       type: 'luxury',
     });
   },
