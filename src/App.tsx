95734d1e251f0352155af65e017aa479f7a0e352
priegelsdev <priegelsdev@gmail.com>
2023-02-28 14:41:15 +0100

    update folder structure for host pages and initialize those files

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,14 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 //pages
 import Home from './pages/Home';
 import About from './pages/About';
-import Rides from './pages/Rides';
+import Rides from './pages/Rides/Rides';
 
 //layout
 import Layout from './Components/Layout';
 
 import '../server';
-import RideDetail from './pages/RideDetail';
+import RideDetail from './pages/Rides/RideDetail';
 
-// TODO: figure out app structure and where to actually put the routing; App.tsx probably correct, but not like this
 export default function App() {
   return (
     <BrowserRouter>
