71c99a4d5d6fb00c6c117f0c12afb1b2399362ea
priegelsdev <priegelsdev@gmail.com>
2023-02-28 09:55:57 +0100

    add burger menu to header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,25 @@
 import { Link, NavLink } from 'react-router-dom';
+import { FaBars } from 'react-icons/fa';
 
 export default function Header() {
+  function toggleNav() {
+    document.getElementById('navbar')?.classList.toggle('hidden');
+  }
+
   return (
-    <header className="bg-primary py-10 px-6 flex justify-between items-center text-white font-inter">
-      <h1 className="text-2xl font-black drop-shadow-md">#SKYRIDES</h1>
-      <nav className="flex gap-5 font-medium">
+    <header className="relative bg-primary flex justify-between items-center text-white font-inter py-10 px-6">
+      <h1 className="text-2xl font-black drop-shadow-md mr-2">#SKYRIDES</h1>
+      <button
+        onClick={toggleNav}
+        id="navbar-btn"
+        className="md:hidden absolute mt-1/2 right-5"
+      >
+        <FaBars size={30} />
+      </button>
+      <nav
+        id="navbar"
+        className="hidden md:flex flex flex-col gap-3 text-lg font-medium mr-12 md:mr-0 md:flex-row"
+      >
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
         <NavLink to="/rides">Rides</NavLink>
