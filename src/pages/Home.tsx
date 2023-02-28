6ad29e1c401c50569d015276b1196ff2e2073a1b
priegelsdev <priegelsdev@gmail.com>
2023-02-28 15:50:13 +0100

    adjust home styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,14 +6,14 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div className="h-full bg-hero bg-cover bg-center font-inter flex flex-col px-6">
-      <h1 className="text-center text-[3rem] font-extrabold py-10 leading-snug drop-shadow-xl">
+      <h1 className="text-center text-3xl font-extrabold py-10 leading-snug drop-shadow-xl">
         Fly <span className="text-primary drop-shadow-sm">high</span>, ride{' '}
         <span className="text-primary drop-shadow-sm">easy</span> <br /> with{' '}
         <span className="drop-shadow-[2px_2px_3px_rgba(0,0,0,0.53)] text-accent decoration-primary">
           SkyRides
         </span>
       </h1>
-      <p className="drop-shadow-sm text-xl text-gray-800 font-medium md:text-center bg-gray-200 bg-opacity-40 rounded-xl p-3">
+      <p className="drop-shadow-sm text-gray-800 md:text-lg font-medium md:text-center bg-gray-200 bg-opacity-40 rounded-xl p-3">
         Soar to new heights with SkyRides. Choose from our fleet of comfortable
         and reliable rides to make your journey a breeze. <br />
         Whether you're planning a weekend getaway or a cross-country adventure,
