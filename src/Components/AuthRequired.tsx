74693717655ad3bee22bbb7eb5e2e879e24ea3a8
priegelsdev <priegelsdev@gmail.com>
2023-03-04 17:58:41 +0100

    add logic to handle error when logging in and include navigation on successful login

--- src/Components/AuthRequired.tsx
+++ src/Components/AuthRequired.tsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 export default function AuthRequired() {
   // TODO: testing auth with token
-  const auth = { token: null };
+  const auth = { token: 1 };
 
   if (!auth.token) {
     return <Navigate to="/login" state={{ message: 'Log in required.' }} />;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,9 +2,17 @@ import React, { useState, ChangeEvent, FormEvent } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { logInUser } from '../../api';
 
+// type declaration for login data
 interface LoginData {
   email: string;
   password: string;
+}
+
+// type declaration for error
+interface ErrorData {
+  message: string;
+  statusText: string;
+  status: number;
 }
 
 export default function Login() {
@@ -13,15 +21,32 @@ export default function Login() {
     email: '',
     password: '',
   });
+  // status state for button when form is submitting
+  const [status, setStatus] = useState<string>('idle');
+  // error state for when form throws an error
+  const [error, setError] = useState<ErrorData | null>(null);
 
   // use location state for when user gets rerouted automatically for when not logged in but trying to access protected route
   const location = useLocation();
+  // bring in navigate hook
+  const navigate = useNavigate();
 
+  // function to handle submit and either log user in or throw error; set states here
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     // preventing default form submit action
     e.preventDefault();
+    // when button is clicked, form is submitting and button gets disabled
+    setStatus('submitting');
+    // initialize error state as null to reset potential errors
+    setError(null);
     // calling login function; since returning a promise, chain to get info
-    logInUser(logInData).then((data) => console.log(data));
+    logInUser(logInData)
+      .then((data) => {
+        console.log(data);
+        navigate('/host');
+      })
+      .catch((err) => setError(err))
+      .finally(() => setStatus('idle'));
   }
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
@@ -42,6 +67,12 @@ export default function Login() {
       <h1 className="text-3xl text-accent font-bold text-center">
         Sign in to your account
       </h1>
+      {/* conditionally render out error message if error is thrown */}
+      {error && (
+        <h3 className="text-lg font-mono text-red-500 font-bold text-center -mb-3">
+          {error.message}
+        </h3>
+      )}
       <form onSubmit={handleSubmit} className="flex flex-col">
         <input
           name="email"
@@ -59,8 +90,12 @@ export default function Login() {
           value={logInData.password}
           className="p-2.5 border-gray-500 border border-t-0 rounded-md rounded-t-none"
         ></input>
-        <button className="bg-accentTwo text-primary rounded-md mt-4 py-2">
-          Log in
+        {/* disabled if btn is clicked and while form is submitting; changing text while submitting*/}
+        <button
+          disabled={status === 'submitting'}
+          className="bg-accentTwo text-primary rounded-md mt-4 py-2"
+        >
+          {status === 'submitting' ? 'Logging in...' : 'Log in'}
         </button>
       </form>
     </div>
