b16e5fd8493d3fe5f422e4ccef98820eaf7610e0
priegelsdev <priegelsdev@gmail.com>
2023-03-07 14:18:51 +0100

    fix typescript errors and bring in navigation to conditionally render log in button text and disabled prop

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,21 +8,15 @@ import {
 } from 'react-router-dom';
 import { logInUser } from '../../api';
 
-// type declaration for login data
-interface LoginData {
-  email: string;
-  password: string;
-}
-
-// type declaration for error
-interface ErrorData {
-  message: string;
-  statusText: string;
-  status: number;
+// type declaration for data coming back from action data
+interface LoginResponse {
+  token?: string;
+  error?: string;
 }
 
 // action function to utilize React Router FormData & handle Form submit
-export async function action({ request }) {
+// TODO: any type not ideal for request object and for error..
+export async function action({ request }: any) {
   const formData = await request.formData();
   const email = formData.get('email');
   const password = formData.get('password');
@@ -31,7 +25,7 @@ export async function action({ request }) {
     const data = await logInUser({ email, password });
     localStorage.setItem('loggedin', 'true');
     return data;
-  } catch (err) {
+  } catch (err: any) {
     return { error: err.message };
   }
 }
@@ -42,9 +36,12 @@ export default function Login() {
   const from = location.state?.from || '/host';
   // bring in navigate hook
   const navigate = useNavigate();
+  // bring in navigation hook
+  const navigation = useNavigation();
   // define data as what comes back from action function
-  const data = useActionData();
+  const data = useActionData() as LoginResponse;
   console.log(data);
+  console.log(navigation);
 
   if (data?.token) {
     navigate(from, { replace: true });
@@ -81,10 +78,10 @@ export default function Login() {
         ></input>
         {/* disabled if btn is clicked and while form is submitting; changing text while submitting*/}
         <button
-          disabled={status === 'submitting'}
+          disabled={navigation.state === 'submitting'}
           className="bg-accentTwo text-primary rounded-md mt-4 py-2"
         >
-          {status === 'submitting' ? 'Logging in...' : 'Log in'}
+          {navigation.state === 'submitting' ? 'Logging in...' : 'Log in'}
         </button>
       </Form>
     </div>
