376840d8e484cd538c3d028cc6424c9fde199b4f
priegelsdev <priegelsdev@gmail.com>
2023-03-08 14:53:22 +0100

    conditionally render login screen; dont render when user is already logged in

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import {
+  Link,
   useLocation,
   useActionData,
   useNavigate,
@@ -30,6 +32,8 @@ export async function action({ request }: any) {
 }
 
 export default function Login() {
+  // define data as what comes back from action function
+  const data = useActionData() as LoginResponse;
   // use location state for when user gets rerouted automatically for when not logged in but trying to access protected route
   const location = useLocation();
   const from = location.state?.from || '/host';
@@ -37,52 +41,68 @@ export default function Login() {
   const navigate = useNavigate();
   // bring in navigation hook
   const navigation = useNavigation();
-  // define data as what comes back from action function
-  const data = useActionData() as LoginResponse;
   console.log(data);
   console.log(navigation);
+  console.log(location);
+  console.log(from);
 
   if (data?.token) {
     navigate(from, { replace: true });
   }
 
-  return (
-    <div className="flex flex-col px-6 gap-6 md:w-[50%] lg:w-[40%] md:m-auto">
-      {location.state?.message && (
-        <h3 className="text-lg font-mono text-red-500 font-bold text-center -mb-3">
-          {location.state.message}
-        </h3>
-      )}
-      <h1 className="text-3xl text-accent font-bold text-center">
-        Sign in to your account
-      </h1>
-      {/* conditionally render out error message if error is thrown */}
-      {data?.error && (
-        <h3 className="text-lg font-mono text-red-500 font-bold text-center -mb-3">
-          {data.error}
-        </h3>
-      )}
-      <Form action="/login" method="post" className="flex flex-col">
-        <input
-          name="email"
-          type="email"
-          placeholder="Email address"
-          className="p-2.5 border-gray-500 border rounded-md rounded-b-none"
-        ></input>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          className="p-2.5 border-gray-500 border border-t-0 rounded-md rounded-t-none"
-        ></input>
-        {/* disabled if btn is clicked and while form is submitting; changing text while submitting*/}
-        <button
-          disabled={navigation.state === 'submitting'}
-          className="bg-accentTwo text-primary rounded-md mt-4 py-2"
+  if (!localStorage.getItem('loggedin', 'true')) {
+    return (
+      <div className="flex flex-col px-6 gap-6 md:w-[50%] lg:w-[40%] md:m-auto">
+        {location.state?.message && (
+          <h3 className="text-lg font-mono text-red-500 font-bold text-center -mb-3">
+            {location.state.message}
+          </h3>
+        )}
+        <h1 className="text-3xl text-accent font-bold text-center">
+          Sign in to your account
+        </h1>
+        {/* conditionally render out error message if error is thrown */}
+        {data?.error && (
+          <h3 className="text-lg font-mono text-red-500 font-bold text-center -mb-3">
+            {data.error}
+          </h3>
+        )}
+        <Form action="/login" method="post" className="flex flex-col">
+          <input
+            name="email"
+            type="email"
+            placeholder="Email address"
+            className="p-2.5 border-gray-500 border rounded-md rounded-b-none"
+          ></input>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="p-2.5 border-gray-500 border border-t-0 rounded-md rounded-t-none"
+          ></input>
+          {/* disabled if btn is clicked and while form is submitting; changing text while submitting*/}
+          <button
+            disabled={navigation.state === 'submitting'}
+            className="bg-accentTwo text-primary rounded-md mt-4 py-2"
+          >
+            {navigation.state === 'submitting' ? 'Logging in...' : 'Log in'}
+          </button>
+        </Form>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col px-6 gap-6 md:w-[50%] lg:w-[40%] md:m-auto">
+        <h1 className="text-3xl text-accent font-bold text-center">
+          Already logged in
+        </h1>
+        <Link
+          to="/host"
+          className="bg-secondary text-center rounded-md py-2 px-4"
         >
-          {navigation.state === 'submitting' ? 'Logging in...' : 'Log in'}
-        </button>
-      </Form>
-    </div>
-  );
+          Go to Host
+        </Link>
+      </div>
+    );
+  }
 }
