0bad72085f34cfe8a80049b5170988765e02676d
priegelsdev <priegelsdev@gmail.com>
2023-03-09 12:25:00 +0100

    fill dashboard with income content and add link to income

--- src/pages/Host/Dashboard.tsx
+++ src/pages/Host/Dashboard.tsx
@@ -1,7 +1,27 @@
+import { BsStarFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+
 export default function Dashboard() {
   return (
-    <div>
-      <h1>dashboard here</h1>
+    <div className="-mx-6">
+      <div className="flex justify-between items-center bg-accent text-gray-800 p-6">
+        <div className="flex flex-col gap-3">
+          <h1 className="text-3xl font-bold">Welcome!</h1>
+          <h5>
+            Income last <span className="font-bold underline">30 days</span>
+          </h5>
+          <h2 className="text-4xl font-bold">$3,840</h2>
+        </div>
+        <Link to="income">Details</Link>
+      </div>
+      <div className="flex items-center">
+        <h2 className="text-xl font-bold">Review score</h2>
+        <BsStarFill className="text-[#FF8C38] text-2xl ml-4 mr-1" />
+        <p>
+          <span className="font-bold">5.0</span>/5
+        </p>
+      </div>
+      <div></div>
     </div>
   );
 }
