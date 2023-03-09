0141176a6ea6086c97a1f860d907b1decc5e99af
priegelsdev <priegelsdev@gmail.com>
2023-03-09 12:30:55 +0100

    fill page more with review and start all listed rides div

--- src/pages/Host/Dashboard.tsx
+++ src/pages/Host/Dashboard.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Dashboard() {
   return (
     <div className="-mx-6">
-      <div className="flex justify-between items-center bg-accent text-gray-800 p-6">
+      <div className="flex justify-between items-center bg-accent text-gray-800 px-6 py-10">
         <div className="flex flex-col gap-3">
           <h1 className="text-3xl font-bold">Welcome!</h1>
           <h5>
@@ -14,14 +14,20 @@ export default function Dashboard() {
         </div>
         <Link to="income">Details</Link>
       </div>
-      <div className="flex items-center">
-        <h2 className="text-xl font-bold">Review score</h2>
-        <BsStarFill className="text-[#FF8C38] text-2xl ml-4 mr-1" />
-        <p>
-          <span className="font-bold">5.0</span>/5
-        </p>
+      <div className="flex justify-between items-center bg-[#dac68c] text-gray-800 px-6 py-10">
+        <div className="flex items-center">
+          <h2 className="text-xl font-bold">Review score</h2>
+          <BsStarFill className="text-[#FF8C38] text-2xl ml-4 mr-1" />
+          <p>
+            <span className="font-bold">5.0</span>/5
+          </p>
+        </div>
+        <Link to="reviews">Details</Link>
       </div>
-      <div></div>
+      <div className="flex justify-between items-center px-6 py-10">
+        <h2 className="text-xl font-bold">Your listed rides</h2>
+        <Link to="rides">View all</Link>
+      </div>
     </div>
   );
 }
